fa4d35ba1a30eaa851138eae95c481d03b1073d8
keva damar galih <kevadamarg@gmail.com>
2020-12-29 22:04:40 +0700

    first commit

---
+++ build/classes/pilketos/koneksi/koneksi.java
@@ -0,0 +1,31 @@
+package pilketos.koneksi;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author kevadamargalih
+ */
+import java.sql.*;
+
+public class koneksi {
+    private static Connection connection = null;
+        public static Connection getConnection() {
+            if (connection != null) 
+                return connection;
+            else {
+                String dbUrl = "jdbc:mysql://localhost:8889/pilketos_uas?user=root&password=root&autoReconnect=true&useSSL=false";
+                try {
+                    Class.forName("com.mysql.jdbc.Driver");
+                    connection=DriverManager.getConnection(dbUrl);
+                } catch (ClassNotFoundException | SQLException e) {
+                }
+            return connection;
+        }
+    }
+}
